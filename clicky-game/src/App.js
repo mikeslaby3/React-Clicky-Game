64bb3860b5719aac9efc6cf6754343c930c5389b
Mike Slaby <mikeslaby3@gmail.com>
2019-07-24 15:01:36 -0700

    Added function that shuffles players when clicked

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,70 +1,103 @@
 import React, { Component } from "react";
 import Player from "./components/Player";
-// import Benny from './images/b'
 
 class App extends Component {
   state = {
     score: 0,
-    hasBeenClicked: false
+    playerImages: [
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/643217.jpg",
+        name: "Andrew Benintendi",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/571788.jpg",
+        name: "Brock Holt",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/656308.jpg",
+        name: "Michael Chavis",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/598265.jpg",
+        name: "Jackie Bradley Jr.",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/502110.jpg",
+        name: "J.D. Martinez",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/605141.jpg",
+        name: "Mookie Betts",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/519144.jpg",
+        name: "Rick Porcello",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/456034.jpg",
+        name: "David Price",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/646240.jpg",
+        name: "Rafeal Devers",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/519242.jpg",
+        name: "Chris Sale",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/543877.jpg",
+        name: "Christian Vasquez",
+        hasBeenClicked: false
+      },
+      {
+        image: "https://securea.mlb.com/mlb/images/players/head_shot/593428.jpg",
+        name: "Xander Bogaerts",
+        hasBeenClicked: false
+      }
+    ]
   };
 
-  playerImages = [
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/643217.jpg",
-      name: "Andrew Benintendi"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/571788.jpg",
-      name: "Brock Holt"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/656308.jpg",
-      name: "Michael Chavis"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/598265.jpg",
-      name: "Jackie Bradley Jr."
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/502110.jpg",
-      name: "J.D. Martinez"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/605141.jpg",
-      name: "Mookie Betts"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/519144.jpg",
-      name: "Rick Porcello"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/456034.jpg",
-      name: "David Price"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/646240.jpg",
-      name: "Rafeal Devers"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/519242.jpg",
-      name: "Chris Sale"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/543877.jpg",
-      name: "Christian Vasquez"
-    },
-    {
-      image: "https://securea.mlb.com/mlb/images/players/head_shot/593428.jpg",
-      name: "Xander Bogaerts"
-    }
-  ];
+  shufflePlayers = (playerArray) => {
+    let ctr = playerArray.length;
+    let temp;
+    let index;
 
-  handlePlayerClick = () => {
+    while (ctr > 0) {
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      temp = playerArray[ctr];
+      playerArray[ctr] = playerArray[index];
+      playerArray[index] = temp;
+    }
+
+    return playerArray;
+  }
+
+  handlePlayerClick = (currentPlayer) => {
+    this.shufflePlayers(this.state.playerImages);
     let score = this.state.score;
-    score++;
-    this.setState({
-      score: score
-    });
+
+    if (currentPlayer.hasBeenClicked) {
+      this.setState({
+        score: 0
+      })
+    } else {
+      score++
+      this.setState({
+        score: score
+      })
+    }
   };
 
   renderImage = (imageURL, imageAltText) => {
@@ -86,10 +119,10 @@ class App extends Component {
     
     const players = (
       <div className="row">
-        {this.playerImages.map((player, index) => {
+        {this.state.playerImages.map((player, index) => {
           return (
             <Player
-              click={this.handlePlayerClick}
+              click={() => this.handlePlayerClick(player)}
               playerImage={this.renderImage(player.image, player.name)}
             />
           );
@@ -107,6 +140,7 @@ class App extends Component {
         <div className="jumbotron">
           <h1>Boston Red Sox Shuffle</h1>
           <h2>Score: {this.state.score} </h2>
+          <h2>High Score: </h2>
         </div>
         <div className="container" style={containerStyle}>
           {players}
