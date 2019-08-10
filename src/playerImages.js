bc974bd69c467003a37781970ad158821e155dcd
Mike Slaby <mikeslaby3@gmail.com>
2019-08-09 17:43:24 -0700

    Put player image object into its own JS file

---
+++ src/playerImages.js
@@ -0,0 +1,52 @@
+const playerImages = [
+    {
+        image: "benny.jpg",
+        name: "Andrew Benintendi"
+    },
+    {
+        image: "brock.jpg",
+        name: "Brock Holt"
+    },
+    {
+        image: "chavis.jpg",
+        name: "Michael Chavis"
+    },
+    {
+        image: "jbj.jpg",
+        name: "Jackie Bradley Jr."
+    },
+    {
+        image: "jdmartinez.jpg",
+        name: "J.D. Martinez"
+    },
+    {
+        image: "mookie.jpg",
+        name: "Mookie Betts"
+    },
+    {
+        image: "porcello.jpg",
+        name: "Rick Porcello"
+    },
+    {
+        image: "price.jpg",
+        name: "David Price"
+    },
+    {
+        image: "rafi.jpg",
+        name: "Rafeal Devers"
+    },
+    {
+        image: "sale.jpg",
+        name: "Chris Sale"
+    },
+    {
+        image: "vazquez.jpg",
+        name: "Christian Vasquez"
+    },
+    {
+        image: "xander.jpg",
+        name: "Xander Bogaerts"
+    }
+];
+
+export default playerImages;
